4a928deb709bccde2a2b9ff9611e1066b5b3a4a2
Kirill <k.d.kuznetsov@stud.mospolytech.ru>
2024-06-24 01:51:26 +0300

    3.0

--- Assets/Scripts/Collectable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Collectable : MonoBehaviour
-{
-    [Range(0,100)][SerializeField] protected int valuable = 10;
-
-    public int Value => valuable;
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Orbitium-v2
